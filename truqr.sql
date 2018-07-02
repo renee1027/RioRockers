51fefd4f643a04432f2295c6d21f580ce7fb1be1
renee1027 <reneex.1027@gmail.com>
2018-07-02 20:12:01 +0200

    edit sql

--- truqr.sql
+++ truqr.sql
@@ -25,10 +25,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `locations`
+-- Table structure for table `Locations`
 --
 
-CREATE TABLE `locations` (
+CREATE TABLE `Locations` (
   `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
@@ -39,10 +39,10 @@ CREATE TABLE `locations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `locations`
+-- Dumping data for table `Locations`
 --
 
-INSERT INTO `locations` (`id`, `name`, `address`, `createdAt`, `updatedAt`, `lat`, `lng`) VALUES
+INSERT INTO `Locations` (`id`, `name`, `address`, `createdAt`, `updatedAt`, `lat`, `lng`) VALUES
 (1, 'edeka theresie', 'Theresienhöhe 12, 80339 München', '2018-06-12 21:51:45', '2018-06-12 21:51:45', 48.133579, 11.542819),
 (2, 'Pasing Arcaden', 'Pasinger Bahnhofspl. 5, 81241 München', '2018-06-18 18:48:57', '2018-06-18 18:48:57', 48.148300, 11.461665),
 (3, 'Mona München', 'Pelkovenstraße 143-147, 80992 München', '2018-06-18 20:40:38', '2018-06-18 20:40:38', 48.182438, 11.530620),
@@ -55,10 +55,10 @@ INSERT INTO `locations` (`id`, `name`, `address`, `createdAt`, `updatedAt`, `lat
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ramps`
+-- Table structure for table `Ramps`
 --
 
-CREATE TABLE `ramps` (
+CREATE TABLE `Ramps` (
   `id` int(11) NOT NULL,
   `occupiedSince` datetime DEFAULT NULL,
   `timesBooked` int(11) NOT NULL DEFAULT '0',
@@ -73,10 +73,10 @@ CREATE TABLE `ramps` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `ramps`
+-- Dumping data for table `Ramps`
 --
 
-INSERT INTO `ramps` (`id`, `occupiedSince`, `timesBooked`, `createdAt`, `updatedAt`, `rampNumber`, `locationId`, `lat`, `lng`, `totalHoursBooked`, `waitingList`) VALUES
+INSERT INTO `Ramps` (`id`, `occupiedSince`, `timesBooked`, `createdAt`, `updatedAt`, `rampNumber`, `locationId`, `lat`, `lng`, `totalHoursBooked`, `waitingList`) VALUES
 (1, NULL, 3, '2018-06-12 22:06:13', '2018-07-01 15:26:58', 1, 1, 48.133579, 11.542819, 0.0000, 0),
 (2, NULL, 0, '2018-06-18 18:42:46', '2018-06-18 18:42:46', 2, 1, 48.133579, 11.542819, 0.0000, 0),
 (3, NULL, 0, '2018-06-18 20:41:53', '2018-06-18 19:05:53', 1, 3, 48.182697, 11.531464, 0.0000, 0),
@@ -92,15 +92,15 @@ INSERT INTO `ramps` (`id`, `occupiedSince`, `timesBooked`, `createdAt`, `updated
 --
 
 --
--- Indexes for table `locations`
+-- Indexes for table `Locations`
 --
-ALTER TABLE `locations`
+ALTER TABLE `Locations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `ramps`
+-- Indexes for table `Ramps`
 --
-ALTER TABLE `ramps`
+ALTER TABLE `Ramps`
   ADD PRIMARY KEY (`id`),
   ADD KEY `Ramps_locationId_foreign_idx` (`locationId`);
 
@@ -109,15 +109,15 @@ ALTER TABLE `ramps`
 --
 
 --
--- AUTO_INCREMENT for table `locations`
+-- AUTO_INCREMENT for table `Locations`
 --
-ALTER TABLE `locations`
+ALTER TABLE `Locations`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `ramps`
+-- AUTO_INCREMENT for table `Ramps`
 --
-ALTER TABLE `ramps`
+ALTER TABLE `Ramps`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
@@ -125,10 +125,10 @@ ALTER TABLE `ramps`
 --
 
 --
--- Constraints for table `ramps`
+-- Constraints for table `Ramps`
 --
-ALTER TABLE `ramps`
-  ADD CONSTRAINT `Ramps_locationId_foreign_idx` FOREIGN KEY (`locationId`) REFERENCES `locations` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `Ramps`
+  ADD CONSTRAINT `Ramps_locationId_foreign_idx` FOREIGN KEY (`locationId`) REFERENCES `Locations` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
